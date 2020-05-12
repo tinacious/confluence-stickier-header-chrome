ea45c7c70de5fa7ba3b7b4384352f4bfc3a66820
Tina Holly <info@tinaciousdesign.com>
2020-05-11 23:41:22 -0700

    "product design" lolz 🩹

--- bundle.sh
+++ bundle.sh
@@ -3,5 +3,6 @@ rm -rf confluence-stickier-header
 mkdir confluence-stickier-header
 cp manifest.json confluence-stickier-header/manifest.json
 cp stickier-header-confluence.js confluence-stickier-header/stickier-header-confluence.js
+cp icon.png confluence-stickier-header/icon.png
 
 zip -r confluence-stickier-header.zip confluence-stickier-header
